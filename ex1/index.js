78f3bf9856615360119be4e7fd1ca67cca0a1279
fukorpse <150160314+fukorpse@users.noreply.github.com>
2024-03-13 22:30:51 -0300

    Update index.js

--- ex1/index.js
+++ ex1/index.js
@@ -1,6 +1,3 @@
-//Implemente uma função que remova todos os elementos até encontrar um número par
-//(inclusive). Verifique os elementos a partir do início do vetor.
-//Considere o seguinte vetor [1, 3, 5, 7, 9, 12, 15, 17, 19, 21].
 let v = [1, 3, 5, 7, 9, 12, 15, 17, 19, 21];
 
 alert(v);
