3b54a665f7742d27543e109d6fd565ce31f9d00a
fukorpse <150160314+fukorpse@users.noreply.github.com>
2024-03-13 22:31:07 -0300

    Update index.js

--- ex2/index.js
+++ ex2/index.js
@@ -1,6 +1,3 @@
-//Implemente uma função que remova todos os elementos até encontrar
-//um número múltiplo de 5 (inclusive). Verifique os elementos a partir
-//do final do vetor. Considere o seguinte vetor [1, 3, 5, 7, 9, 12, 15, 17, 19, 21].
 let v = [1, 3, 5, 7, 9, 12, 15, 17, 19, 21];
 
 alert(v);
